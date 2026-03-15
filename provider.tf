dc27b2b1e5a5cd9aa86e490e601089ac79022426
NEEL KAMAL <neelk6252@gmail.com>
2026-03-15 22:47:44 +0530

    updated backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "neel-terraform-state"
+    key    = "/state/terraform.tfstate"
+    region = "ap-south-1"
+    
+  }
 }
 
 # Configure the AWS Provider
